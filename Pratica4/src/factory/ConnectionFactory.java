76fd07a30ad9074e34f19d97f894ce38d8d1baa6
Kamille Fernandes <kamillefds2005@gmail.com>
2026-04-15 11:41:03 -0300

    feat: inicio da configuração da conexao do banco

--- Pratica4/src/factory/ConnectionFactory.java
+++ Pratica4/src/factory/ConnectionFactory.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package factory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author Fatec
  */
 public class ConnectionFactory {
-    
+    public Connection getConnection(){
+        try{
+            return DriverManager.getConnection("jdbc:mysql://localhost/bd_crud","root","fatec");
+        }
+        catch(SQLException excecao){
+            throw new RuntimeException(excecao);
+        }
+    }
 }
